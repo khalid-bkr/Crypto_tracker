698ad3b11684a900973e13f0cd404faa181f5385
khalid abubaker <khalid7.bkr@gmail.com>
2021-11-20 02:40:17 -0800

    monkey patch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Fragment, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Coin from "./Coin";
 import "./Coin.css";
